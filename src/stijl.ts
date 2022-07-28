501e442158088b1bf920eb98da4413fa97b61d9c
Dan McAtee <danielmcatee@me.com>
2022-07-28 03:01:49 +0200

    add exports from config and globalStyles

--- src/stijl.ts
+++ src/stijl.ts
@@ -1,3 +1,14 @@
+import { format } from 'path';
+
 export * from './components/Box';
 export * from './components/Typography';
 export * from './components/Flex';
+export {
+  css,
+  getCssText,
+  keyframes,
+  styled,
+  theme,
+  config,
+} from './lib/stitches.config';
+export { globalStyles } from './lib/globalStyles';
